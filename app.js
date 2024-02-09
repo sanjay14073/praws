9d899a842a3193bb7a5abba415cad0d8885ca4d0
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2024-02-09 17:58:32 +0530

    try3

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/getQR',async (req,res)=>{
     img_svg.pipe(fs.createWriteStream(path.join(__dirname, `/uploads/${uniqueid}.png`)))
     res.render("showQR",{imgurl:`uploads/${uniqueid}.png`});
 })
-PORT=process.env.PORT||3000;
+PORT=process.env.PORT||80;
 app.listen(PORT,(req,res)=>{
-    console.log('server started a port 3000')
+    console.log('server started a port 80')
 })
